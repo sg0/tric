21d0ecac553444ac728352f65812575d20268eee
sg0 <sg0@pnnl.gov>
2020-07-23 11:26:30 -0700

    updates

--- dfastric.hpp
+++ dfastric.hpp
@@ -46,8 +46,9 @@
 #include <utility>
 #include <cstring>
 #include <iomanip>
+#include <limits>
 
-#define PACK_SIZE (64)
+#define PAD_SIZE (1000)
 
 class TriangulateAggrFatDtype
 {
@@ -55,8 +56,10 @@ class TriangulateAggrFatDtype
 
         TriangulateAggrFatDtype(Graph* g): 
             g_(g), sbuf_ctr_(nullptr), sbuf_disp_(nullptr), 
-            out_ghosts_(0), in_ghosts_(0), nghosts_(0), sbuf_(nullptr), 
-            send_counts_(0), recv_counts_(0), ntriangles_(0)  
+            out_ghosts_(0), in_ghosts_(0), nghosts_(0), sbuf_(nullptr), rbuf_(nullptr), 
+            send_counts_(nullptr), recv_counts_(nullptr), ntriangles_(0),
+            sdispls_(nullptr), rdispls_(nullptr), scnts_(nullptr), rcnts_(nullptr),
+            send_t_(nullptr), recv_t_(nullptr), rptr_(nullptr)
         {
             comm_ = g_->get_comm();
             MPI_Comm_size(comm_, &size_);
@@ -64,7 +67,14 @@ class TriangulateAggrFatDtype
             send_counts_ = new GraphElem[size_];
             recv_counts_ = new GraphElem[size_];
             sbuf_ctr_ = new GraphElem[size_];
+            sdispls_ = new int[size_];
+            rdispls_ = new int[size_];
+            scnts_ = new int[size_];
+            rcnts_ = new int[size_];
+            send_t_ = new MPI_Datatype[size_];
+            recv_t_ = new MPI_Datatype[size_];
             sbuf_disp_ = new GraphElem[size_];
+            rptr_ = new GraphElem[size_+1];
             std::memset(send_counts_, 0, sizeof(GraphElem)*size_);
             std::memset(recv_counts_, 0, sizeof(GraphElem)*size_);
             std::memset(sbuf_ctr_, 0, sizeof(GraphElem)*size_);
@@ -92,18 +102,52 @@ class TriangulateAggrFatDtype
             {
                 out_ghosts_ += send_counts_[p];
                 in_ghosts_ += recv_counts_[p];
-                send_counts_[p] = roundUp(send_counts_[p]*2, (GraphElem)PACK_SIZE); 
             }
             nghosts_ = out_ghosts_ + in_ghosts_;
-            MPI_Alltoall(send_counts_, 1, MPI_GRAPH_TYPE, recv_counts_, 1, MPI_GRAPH_TYPE, comm_);
-            GraphElem pos = 0;
+            GraphElem spos = 0, rpos = 0;
             for (int p = 0; p < size_; p++)
             {
-                sbuf_disp_[p] = pos;
-                pos += send_counts_[p];
+                sbuf_disp_[p] = spos;
+                sdispls_[p] = (int)(spos*sizeof(GraphElem));
+                if ((send_counts_[p]*2) < std::numeric_limits<int>::max())
+                {
+                    MPI_Type_contiguous(1, MPI_GRAPH_TYPE, &send_t_[p]);
+                    send_counts_[p] *= 2;
+                    scnts_[p] = send_counts_[p];
+                }
+                else 
+                {
+                    send_counts_[p] = roundUp(send_counts_[p]*2, PAD_SIZE);
+                    MPI_Type_contiguous(PAD_SIZE, MPI_GRAPH_TYPE, &send_t_[p]);
+                    scnts_[p] = (int)((GraphElem)(send_counts_[p] / (GraphElem)PAD_SIZE));
+                }
+                spos += send_counts_[p];
+                MPI_Type_commit(&send_t_[p]);
             }
-            sbuf_ = new GraphElem[pos];
-            std::memset(sbuf_, 0, pos*sizeof(GraphElem));
+            sbuf_ = new GraphElem[spos];
+            std::memset(sbuf_, 0, spos*sizeof(GraphElem));
+            MPI_Alltoall(send_counts_, 1, MPI_GRAPH_TYPE, recv_counts_, 1, MPI_GRAPH_TYPE, comm_);
+            for (int p = 0; p < size_; p++) 
+            {
+                rptr_[p] = rpos;
+                rdispls_[p] = (int)(rpos*sizeof(GraphElem));
+                if ((recv_counts_[p]*2) < std::numeric_limits<int>::max())
+                {
+                    MPI_Type_contiguous(1, MPI_GRAPH_TYPE, &recv_t_[p]);
+                    recv_counts_[p] *= 2;
+                    rcnts_[p] = recv_counts_[p];
+                }
+                else 
+                {
+                    recv_counts_[p] = roundUp(recv_counts_[p]*2, PAD_SIZE);
+                    MPI_Type_contiguous(PAD_SIZE, MPI_GRAPH_TYPE, &recv_t_[p]);
+                    rcnts_[p] = (int)((GraphElem)(recv_counts_[p] / (GraphElem)PAD_SIZE));
+                }
+                rpos += recv_counts_[p];
+                MPI_Type_commit(&recv_t_[p]);
+            }
+            rptr_[size_] = rpos;
+            rbuf_ = new GraphElem[rpos];
         }
 
         ~TriangulateAggrFatDtype() {}
@@ -111,10 +155,23 @@ class TriangulateAggrFatDtype
         void clear()
         {
             delete []sbuf_;
+            delete []rbuf_;
             delete []sbuf_ctr_;
             delete []sbuf_disp_;
+            delete []rptr_;
             delete []send_counts_;
             delete []recv_counts_;
+            delete []sdispls_;
+            delete []rdispls_;
+            delete []scnts_;
+            delete []rcnts_;
+            for (int p = 0; p < size_; p++)
+            {
+                MPI_Type_free(&send_t_[p]);
+                MPI_Type_free(&recv_t_[p]);
+            }
+            delete []send_t_;
+            delete []recv_t_;
         }
 
         // TODO
@@ -184,43 +241,20 @@ class TriangulateAggrFatDtype
             lookup_edges();
             MPI_Barrier(comm_);
             // communication step 1
-            GraphElem spos=0, rpos=0, dpos=0;
-            int *sdispls = new int[size_];
-            int *rdispls = new int[size_];
-            GraphElem *rptr = new GraphElem[size_+1];
             GraphElem *rinfo = new GraphElem[size_*2];
             GraphElem *srinfo = new GraphElem[size_*2];
             std::memset(srinfo, 0, sizeof(GraphElem)*size_*2);
             std::memset(rinfo, 0, sizeof(GraphElem)*size_*2);
-            int *scnts = new int[size_];
-            int *rcnts = new int[size_];
-            MPI_Datatype pack_t;
-            MPI_Type_contiguous(PACK_SIZE, MPI_GRAPH_TYPE, &pack_t);
-            MPI_Type_commit(&pack_t);
-            for (int p = 0; p < size_; p++) 
-            {
-                rptr[p] = dpos;
-                sdispls[p] = spos;
-                rdispls[p] = rpos;
-                scnts[p] = (int)((GraphElem)(send_counts_[p] / (GraphElem)PACK_SIZE));
-                rcnts[p] = (int)((GraphElem)(recv_counts_[p] / (GraphElem)PACK_SIZE));
-                spos += scnts[p];
-                rpos += rcnts[p];
-                dpos += recv_counts_[p];
-            }
-            GraphElem *rbuf = new GraphElem[dpos];
-            rptr[size_] = dpos;
-            MPI_Barrier(comm_);
-            MPI_Alltoallv(sbuf_, scnts, sdispls, pack_t, 
-                    rbuf, rcnts, rdispls, pack_t, comm_);
+            MPI_Alltoallw(sbuf_, scnts_, sdispls_, send_t_, 
+                    rbuf_, rcnts_, rdispls_, recv_t_, comm_);
             // EDGE_SEARCH_TAG
             GraphElem tup[2];
             for (int p = 0; p < size_; p++)
             {
-                for (GraphElem k = rptr[p]; k < rptr[p+1]; k+=2)
+                for (GraphElem k = rptr_[p]; k < rptr_[p+1]; k+=2)
                 {
-                    tup[0] = rbuf[k];
-                    tup[1] = rbuf[k+1];
+                    tup[0] = rbuf_[k];
+                    tup[1] = rbuf_[k+1];
                     if (check_edgelist(tup))
                         rinfo[p*2] += 1;   // 0 == EDGE_VALID_TAG 
                     else 
@@ -236,15 +270,8 @@ class TriangulateAggrFatDtype
                 ntriangles_ += srinfo[p*2];
                 nghosts_ -= srinfo[p*2+1];
             }
-            MPI_Type_free(&pack_t);
-            delete []sdispls;
-            delete []rdispls;
             delete []rinfo;
             delete []srinfo;
-            delete []rbuf;
-            delete []scnts;
-            delete []rcnts;
-            delete []rptr;
             GraphElem ttc = 0, ltc = ntriangles_;
             MPI_Barrier(comm_);
             MPI_Reduce(&ltc, &ttc, 1, MPI_GRAPH_TYPE, MPI_SUM, 0, comm_);
@@ -254,9 +281,11 @@ class TriangulateAggrFatDtype
         Graph* g_;
         GraphElem ntriangles_;
         GraphElem out_ghosts_, in_ghosts_, nghosts_;
-	GraphElem *sbuf_, *sbuf_ctr_, *sbuf_disp_;
+	GraphElem *sbuf_, *rbuf_, *sbuf_ctr_, *sbuf_disp_, *rptr_;
         GraphElem *send_counts_, *recv_counts_;
 	int rank_, size_;
         MPI_Comm comm_;
+        int *sdispls_, *rdispls_, *scnts_, *rcnts_;
+        MPI_Datatype *send_t_, *recv_t_;
 };
 #endif
